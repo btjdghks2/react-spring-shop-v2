c13eca5a686dca5cc9d88fbcfa51111dd61610f3
btjdghks2 <btjdghks2@gmail.com>
2023-06-03 23:47:49 +0900

    lawn care

--- src/main/java/com/example/shopspring/Controller/AdminController.java
+++ src/main/java/com/example/shopspring/Controller/AdminController.java
@@ -1,8 +1,13 @@
 package com.example.shopspring.Controller;
 
+import com.example.shopspring.Entitylist.CategoryEntity.OuterEntity;
 import com.example.shopspring.Service.AdminService;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
 
     private AdminService adminService;
+
+    @PostMapping("/admin/post")
+    public ResponseEntity<?> Outersave(@RequestBody OuterEntity outerEntity) throws IllegalAccessException {
+        return new ResponseEntity<>(adminService.Outersave(outerEntity), HttpStatus.OK);
+    }
 
 
 
--- src/main/java/com/example/shopspring/Service/AdminService.java
+++ src/main/java/com/example/shopspring/Service/AdminService.java
@@ -1,7 +1,11 @@
 package com.example.shopspring.Service;
 
+import com.example.shopspring.Entitylist.CategoryEntity.OuterEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminService {
+
+    public Object Outersave(OuterEntity pageEntity) {
+    }
 }
