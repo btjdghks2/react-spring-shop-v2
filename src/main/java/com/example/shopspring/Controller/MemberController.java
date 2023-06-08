78c188c9287c8c63c0ee4829b215bca17c5610ca
btjdghks2 <btjdghks2@gmail.com>
2023-06-08 21:30:42 +0900

    spring security study1

--- src/main/java/com/example/shopspring/Controller/MemberController.java
+++ src/main/java/com/example/shopspring/Controller/MemberController.java
@@ -1,11 +1,22 @@
 package com.example.shopspring.Controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class MemberController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
 
 
 }
