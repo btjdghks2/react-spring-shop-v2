e736f5b315ec0d8de57ec12b4b3eecdbb78bbb74
btjdghks2 <btjdghks2@gmail.com>
2023-06-13 18:35:43 +0900

    security self test

--- src/main/java/com/example/shopspring/Security/SecurityConfig.java
+++ src/main/java/com/example/shopspring/Security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.shopspring.Security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
@@ -23,94 +24,14 @@ import java.io.IOException;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/user").hasRole("USER")
-                .antMatchers("/admin/pay").hasRole("ADMIN")
-                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
-                .anyRequest().authenticated();
-
-        http // 인증
-                .formLogin()
-
-                .defaultSuccessUrl("/")
-                .failureUrl("/loginPage")
-                .usernameParameter("userId")
-                .passwordParameter("password")
-                .loginProcessingUrl("/login_proc")
-                .successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        System.out.println("authentication" + authentication.getName());
-                        response.sendRedirect("/");
-                    }
-                })
-                .failureHandler(new AuthenticationFailureHandler() {
-                    @Override
-                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-                        System.out.println("exception" + exception.getMessage());
-                        response.sendRedirect("/login");
-                    }
-                })
-                .permitAll();
-
-        http
-                .logout()
-                .logoutUrl("/")
-                .logoutSuccessUrl("/login")
-
-                .addLogoutHandler(new LogoutHandler() {
-                    @Override
-                    public void logout(HttpServletRequest request,
-                                       HttpServletResponse response,
-                                       Authentication authentication) {
-
-                        HttpSession session = request.getSession();
-                        session.invalidate(); //세션 무효화
-
-
-                    }
-                })
-                .logoutSuccessHandler(new LogoutSuccessHandler(){
-                    @Override
-                    public void onLogoutSuccess(HttpServletRequest request,
-                                                HttpServletResponse response,
-                                                Authentication authentication) throws IOException, ServletException {
-                        response.sendRedirect("/login");
-                    }
-                })
-                .deleteCookies("JSESSIONID","remember-me");
-
-        http
-                .sessionManagement()
-                .maximumSessions(5)
-                .maxSessionsPreventsLogin(false);
-
-        http
-                .sessionManagement()
-                .sessionFixation().changeSessionId();
-
-        http
-                .exceptionHandling()
-                .authenticationEntryPoint(new AuthenticationEntryPoint() {
-                    @Override
-                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-                        response.sendRedirect("/login");
-                    }
-                })
-                .accessDeniedHandler(new AccessDeniedHandler() {
-                    @Override
-                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-                        response.sendRedirect("/denied");
-                    }
-                })
-
-
-
-
-
-
+        super.configure(http);
     }
 }
