b3b6c9004019a1fd29ffc8d3b33bb402747f5d4b
btjdghks2 <btjdghks2@gmail.com>
2023-06-19 15:18:01 +0900

    controller setting

--- src/main/java/com/example/shopspring/Controller/ShoppingPageController.java
+++ src/main/java/com/example/shopspring/Controller/ShoppingPageController.java
@@ -1,4 +1,36 @@
 package com.example.shopspring.Controller;
 
+
+import com.example.shopspring.EntityList.Product.ProductEntity;
+import com.example.shopspring.Respositorylist.ProductRepository;
+import com.example.shopspring.Service.ShoppingPageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
 public class ShoppingPageController {
+
+    private final ProductRepository productRepository;
+
+    private final ShoppingPageService shoppingPageService;
+
+
+    @GetMapping("/")
+    public String Mainpage(Model model) {
+        List<ProductEntity> productEntityList = productRepository.findAll();
+
+        model.addAttribute("productindexlist",productEntityList);
+
+
+        return "";
+    }
+
+
+
+
 }
--- src/main/java/com/example/shopspring/Respositorylist/ProductRepository.java
+++ src/main/java/com/example/shopspring/Respositorylist/ProductRepository.java
@@ -1,6 +1,12 @@
 package com.example.shopspring.Respositorylist;
 
+import com.example.shopspring.EntityList.Product.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository {
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    @Override
+    List<ProductEntity> findAll();
 }
