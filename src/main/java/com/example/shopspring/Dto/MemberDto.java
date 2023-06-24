ff9e64132323326f4f5b6d1f6b6933ae1f5e9bc3
btjdghks2 <btjdghks2@gmail.com>
2023-06-24 16:15:00 +0900

    memberDto

---
+++ src/main/java/com/example/shopspring/Dto/MemberDto.java
@@ -0,0 +1,16 @@
+package com.example.shopspring.Dto;
+
+
+
+public class MemberDto {
+
+    private Long id;
+
+    private String ShoppingmalId;
+
+
+    private String membername;
+
+
+    private String memberpassword;
+}
